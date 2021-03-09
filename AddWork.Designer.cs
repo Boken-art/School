a2d2bb54d7df6efa8e1672636633e939cbd63558
Boken-art <72617944+Boken-art@users.noreply.github.com>
2021-03-09 19:53:02 +0100

    allGood

--- AddWork.Designer.cs
+++ AddWork.Designer.cs
@@ -32,28 +32,19 @@ namespace Shool2
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            //this.Work_name = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(135, 101);
+            this.textBox1.Location = new System.Drawing.Point(135, 95);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(203, 20);
             this.textBox1.TabIndex = 0;
             // 
-            // Work name
-            // 
-          /*  this.Work_name.Location = new System.Drawing.Point(29, 101);
-            this.Work_name.Name = "Work name";
-            this.Work_name.Size = new System.Drawing.Size(100, 20);
-            this.Work_name.TabIndex = 1;
-            this.Work_name.Text = "Work name\r\n";*/
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(135, 142);
+            this.button1.Location = new System.Drawing.Point(135, 143);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(203, 48);
             this.button1.TabIndex = 2;
@@ -66,7 +57,6 @@ namespace Shool2
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.button1);
-           // this.Controls.Add(this.Work_name);
             this.Controls.Add(this.textBox1);
             this.Name = "AddWork";
             this.Size = new System.Drawing.Size(395, 559);
